38b766b58e3d2f671eec7380700d455e0fc042bf
Muktaka Joshipura <muktaka2000@gmail.com>
2026-03-12 22:19:59 -0500

    feat(Util.Numbers): Add primeFactorization

--- src/Util/Numbers.hs
+++ src/Util/Numbers.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE InstanceSigs #-}
+
 module Util.Numbers
   ( divisible,
     isPrime,
     allPrimesIn,
     factorial,
+    primeFactorization,
     Digits (..),
     singleDigits,
-    glueDigits
+    glueDigits,
   )
 where
 
@@ -18,6 +20,29 @@ isPrime :: (Integral a) => a -> Bool
 isPrime a
   | a <= 1 = False
   | otherwise = null [x | x <- takeWhile (\n -> n * n <= a) [2 ..], divisible a x]
+
+primes :: (Integral a) => [a]
+primes = filter isPrime [1 ..]
+
+primeFactorization :: (Integral a) => a -> [(a, Int)]
+primeFactorization x
+  | x <= 1 = []
+  | otherwise =
+      let countDivides :: (Integral a) => a -> a -> Int -> (Int, a)
+          countDivides b p accum
+            | b `divisible` p =
+                countDivides (b `div` p) p (1 + accum)
+            | otherwise = (accum, b)
+
+          pfInner :: (Integral a) => a -> [a] -> [(a, Int)]
+          pfInner _ [] = error "Cannot happen"
+          pfInner b (p : ps)
+            | b < p = []
+            | b `divisible` p =
+                let (count, remaining) = countDivides b p 0
+                 in (p, count) : pfInner remaining ps
+            | otherwise = pfInner b ps
+       in pfInner x primes
 
 allPrimesIn :: (Integral a) => [a] -> [a]
 allPrimesIn = filter isPrime
